1ab525bd7bbac2224aaa7efe4e4ef56afa75e56a
github-action-benchmark <github@users.noreply.github.com>
2025-11-16 19:25:10 +0000

    add Benchmark (pytest) benchmark result for 6dae93f6be9c5704f546bfb8cc33770dbdb97974

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1763321110633,
+  "repoUrl": "https://github.com/KoG-teeworlds/teehistorian-py",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "avolicious@kog.tw",
+            "name": "Avolicious",
+            "username": "Avolicious"
+          },
+          "committer": {
+            "email": "avolicious@kog.tw",
+            "name": "Avolicious",
+            "username": "Avolicious"
+          },
+          "distinct": true,
+          "id": "6dae93f6be9c5704f546bfb8cc33770dbdb97974",
+          "message": "fix: versioning",
+          "timestamp": "2025-11-16T20:24:16+01:00",
+          "tree_id": "09dbf93a3d0152073a411bf323cd727e5d06f1c9",
+          "url": "https://github.com/KoG-teeworlds/teehistorian-py/commit/6dae93f6be9c5704f546bfb8cc33770dbdb97974"
+        },
+        "date": 1763321110058,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_benchmarks.py::test_placeholder_benchmark",
+            "value": 72024.24177376906,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000001903858214104065",
+            "extra": "mean: 13.884214194729587 usec\nrounds: 54964"
+          }
+        ]
+      }
+    ]
+  }
+}
