c3e6a0123c17187c0060c56226a9b16e02f42e67
github-action-benchmark <github@users.noreply.github.com>
2025-11-16 19:47:50 +0000

    add Benchmark (pytest) benchmark result for b9407f25174845d3e891615e5ec16babd35a7206

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1763322470885,
+  "repoUrl": "https://github.com/KoG-teeworlds/teehistorian-py",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "avolicious@kog.tw",
+            "name": "Avolicious",
+            "username": "Avolicious"
+          },
+          "committer": {
+            "email": "avolicious@kog.tw",
+            "name": "Avolicious",
+            "username": "Avolicious"
+          },
+          "distinct": true,
+          "id": "b9407f25174845d3e891615e5ec16babd35a7206",
+          "message": "fix: add context manager support",
+          "timestamp": "2025-11-16T20:47:00+01:00",
+          "tree_id": "5311a7dddf1717dd7039a91a1d05c5270c1ce394",
+          "url": "https://github.com/KoG-teeworlds/teehistorian-py/commit/b9407f25174845d3e891615e5ec16babd35a7206"
+        },
+        "date": 1763322470554,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_benchmarks.py::test_placeholder_benchmark",
+            "value": 64018.39377385938,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000011995720471722772",
+            "extra": "mean: 15.620510622813061 usec\nrounds: 59165"
+          }
+        ]
+      }
+    ]
+  }
+}
