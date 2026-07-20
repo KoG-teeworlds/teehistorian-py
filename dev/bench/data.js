62c54326d67eb67ab2463150a3a2a01a9ec51a5d
github-action-benchmark <github@users.noreply.github.com>
2026-07-20 14:19:50 +0000

    add Benchmark (pytest) benchmark result for 8d57933e9bad3a19d8b285eb39ab610b477c6118

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784557190924,
+  "repoUrl": "https://github.com/KoG-teeworlds/teehistorian-py",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "avolicious@kog.tw",
+            "name": "Avolicious",
+            "username": "Avolicious"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d57933e9bad3a19d8b285eb39ab610b477c6118",
+          "message": "Merge pull request #5 from KoG-teeworlds/update-dependencies\n\nchore: update dependencies",
+          "timestamp": "2026-07-20T16:18:54+02:00",
+          "tree_id": "30d5deaaed6d9d1b30560be058e1c39ad079481c",
+          "url": "https://github.com/KoG-teeworlds/teehistorian-py/commit/8d57933e9bad3a19d8b285eb39ab610b477c6118"
+        },
+        "date": 1784557190546,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_benchmarks.py::test_write_chunks",
+            "value": 2528.0559734865733,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000892601290677988",
+            "extra": "mean: 395.560861977612 usec\nrounds: 1891"
+          }
+        ]
+      }
+    ]
+  }
+}
